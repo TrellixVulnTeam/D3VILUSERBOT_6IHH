1ffdb168d7a29324dfce7eaaacad44d0d485a468
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-07-18 18:47:48 +0530

    Update and rename hellbot/plugins/figlet.py to d3vilbot/plugins/figlet.py

--- d3vilbot/plugins/figlet.py
+++ d3vilbot/plugins/figlet.py
@@ -46,8 +46,4 @@ async def figlet(event):
 
 CmdHelp("figlet").add_command(
   "figlet", "text : type", "The types are slant, 3D, 5line, alpha, banner, doh, iso, letter, allig, dotm, bubble, bulb, digi"
-).add_info(
-  "Another Art plugin but figlet."
-).add_warning(
-  "✅ Harmless Module."
 ).add()
