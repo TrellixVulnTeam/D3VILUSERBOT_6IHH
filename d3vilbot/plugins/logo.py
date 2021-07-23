40453437bdd6cf48af39f031206cc92772e72e9a
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-07-23 14:43:40 +0530

    Update logo.py

--- d3vilbot/plugins/logo.py
+++ d3vilbot/plugins/logo.py
@@ -59,7 +59,7 @@ async def lg1(d3vilevent):
     await bot.send_file(
         d3vilevent.chat_id,
         file_name,
-        caption=f"**мα∂ε вү :** {d3vil_mention}/n ωιтн тнε нεℓρ σғ [∂3vιℓ вαcкgяσυη∂'s](t.me/D3VIL_GFX)",
+        caption=f"**мα∂ε вү :** {d3vil_mention}\n ωιтн тнε нεℓρ σғ [∂3vιℓ вαcкgяσυη∂'s](t.me/D3VIL_GFX)",
     )
     await event.delete()
     try:
