8ed88bfeba7989fca99be89aa5acde8a91c3e545
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-07-26 08:25:09 +0530

    Create animationcmnd.py

---
+++ d3vilbot/plugins/animationcmnd.py
@@ -0,0 +1,27 @@
+CmdHelp("animation").add_command(
+  'boxs', None, 'Use and see'
+).add_command(
+  'kiler', '<text>', 'Cool killing animation with name'
+).add_command(
+  'eye', None, 'Use and see'
+).add_command(
+  'thinking', None, 'Use and see'
+).add_command(
+  'snake', None, 'Use and see'
+).add_command(
+  'human', None, 'Use and see'
+).add_command(
+  'mc', None, 'Use and see'
+).add_command(
+  'virus', None, 'Use and see'
+).add_command(
+  'repe', None, 'Use and see'
+).add_command(
+  'nikal', None, 'Use and see'
+).add_command(
+  'music', None, 'Use and see'
+).add_command(
+  'squ', None, 'Use and see'
+).add_command(
+  'rain', None, 'Use and see'
+)add.()
