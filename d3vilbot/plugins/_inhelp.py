d29b60e871cde8325511d98ee858aee429a8cb30
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-07-27 23:10:14 +0530

    Update _inhelp.py

--- d3vilbot/plugins/_inhelp.py
+++ d3vilbot/plugins/_inhelp.py
@@ -136,7 +136,7 @@ if Config.BOT_USERNAME is not None and tgbot is not None:
             ]
 
         elif event.query.user_id == bot.uid and query == "alive":
-            kr_ish = alive_txt.format(Config.ALIVE_MSG, tel_ver, d3vil_ver, uptime, abuse_m, is_sudo, d3vil_mention)
+            kr_ish = alive_txt.format(Config.ALIVE_MSG, d3vil_mention, tel_ver, d3vil_ver, uptime, abuse_m, is_sudo)
             alv_btn = [
                 [Button.url(f"{D3VIL_USER}", f"tg://openmessage?user_id={d3krish}")],
                 [Button.url("𝖬𝗒 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", f"https://t.me/{my_channel}"), 
