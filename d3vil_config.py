59292062c0c5b1c00af699625a9695e60baecb4b
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-08-06 22:28:14 +0530

    Update d3vil_config.py

--- d3vil_config.py
+++ d3vil_config.py
@@ -31,7 +31,10 @@ class Config(object):
     CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     DUAL_LOG = os.environ.get("DUAL_LOG", None)
-    EMOJI_IN_HELP = os.environ.get("EMOJI_IN_HELP", "🔹")
+    EMOJI_IN_HELP = os.environ.get("EMOJI_IN_HELP", "✰")
+    EXCLUDE_WAIFU = os.environ.get("EXCLUDE_WAIFU", None)
+    EXTRA = os.environ.get("EXTRA", None)
+    EXTRA_REPO = os.environ.get("EXTRA_REPO", None)
     FBAN_LOG_GROUP = os.environ.get("FBAN_LOG_GROUP", None)
     if FBAN_LOG_GROUP:
         FBAN_LOG_GROUP = int(FBAN_LOG_GROUP)
