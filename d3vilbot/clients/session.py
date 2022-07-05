5473d20ec678506871583fdd24c906b98ff0c163
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-05 21:40:42 +0530

    Create session.py

---
+++ d3vilbot/clients/session.py
@@ -0,0 +1,92 @@
+import sys
+
+from telethon import TelegramClient
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+
+from d3vilbot.config import Config
+
+
+if Config.D3VILBOT_SESSION:
+    session = StringSession(str(Config.D3VILBOT_SESSION))
+else:
+    session = "d3vilbot"
+
+try:
+    D3vil = TelegramClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"D3VILBOT_SESSION - {e}")
+    sys.exit()
+
+
+if Config.SESSION_2:
+    session2 = StringSession(str(Config.SESSION_2))
+    D2 = TelegramClient(
+        session=session2,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    D2 = None
+
+
+if Config.SESSION_3:
+    session3 = StringSession(str(Config.SESSION_3))
+    D3 = TelegramClient(
+        session=session3,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    D3 = None
+
+
+if Config.SESSION_4:
+    session4 = StringSession(str(Config.SESSION_4))
+    D4 = TelegramClient(
+        session=session4,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    D4 = None
+
+
+if Config.SESSION_5:
+    session5 = StringSession(str(Config.SESSION_5))
+    D5 = TelegramClient(
+        session=session5,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+else:
+    D5 = None
+
+
+D3vilBot = TelegramClient(
+    session="D3vil-TBot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.BOT_TOKEN)
