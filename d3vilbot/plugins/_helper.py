22428a2d34f9bbec499ba7a40db5a5101890fc77
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-06 09:34:35 +0530

    Update _helper.py

--- d3vilbot/plugins/_helper.py
+++ d3vilbot/plugins/_helper.py
@@ -1,67 +1,73 @@
 import asyncio
 import requests
+
 from telethon import functions
 from telethon.errors import ChatSendInlineForbiddenError as noin
-from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot
+from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot, BotInlineDisabledError as noinline, YouBlockedUserError
 
 from . import *
 
 msg = f"""
 **⚡ ℓεgεη∂αяү αғ тεαм ∂3vιℓ ⚡**
-
   •        [⚜️ 𝐑𝐞𝐩𝐨 ⚜️](https://github.com/D3KRISH/D3vilBot)
   •        [⚡ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ⚡](https://t.me/D3VIL_BOT_OFFICIAL)
-
   •  ©️ {d3vil_channel} ™
 """
 botname = Config.BOT_USERNAME
 
-@bot.on(d3vil_cmd(pattern="repo$"))
-@bot.on(sudo_cmd(pattern="repo$", allow_sudo=True))
+@d3vil_cmd(pattern="repo$")
 async def repo(event):
+    cids = await client_id(event)
+    d3vilkrish, D3VIL_USER, d3vill_mention = cids[0], cids[1], cids[2]
     try:
-        d3vil = await bot.inline_query(botname, "repo")
+        d3vill = await event.client.inline_query(botname, "repo")
         await d3vil[0].click(event.chat_id)
-        if event.sender_id == d3krish:
+        if event.sender_id == d3vilkrish:
             await event.delete()
     except (noin, dedbot):
         await eor(event, msg)
 
 
-@bot.on(d3vil_cmd(pattern="help ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="help ?(.*)", allow_sudo=True))
+@d3vil_cmd(pattern="help$")
 async def _(event):
-    if event.fwd_from:
-        return
     tgbotusername = Config.BOT_USERNAME
-    input_str = event.pattern_match.group(1)
-    try:
-        if not input_str == "":
-            if input_str in CMD_HELP:
-                await eor(event, str(CMD_HELP[args]))
-    except:
-        pass
+    chat = "@Botfather"
     if tgbotusername is not None:
-        results = await event.client.inline_query(tgbotusername, "d3vilbot_d3vlp")
-        await results[0].click(
-            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-        )
-        await event.delete()
+        try:
+            results = await event.client.inline_query(tgbotusername, "d3vilbot_help")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        except noinline:
+            d3vil = await eor(event, "**Inline Mode is disabled.** \n__Turning it on, please wait for a minute...__")
+            async with bot.conversation(chat) as conv:
+                try:
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(tgbotusername)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message(perf)
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    return await d3vil.edit("Unblock @Botfather first.")
+                await d3vil.edit(f"**Turned On Inline Mode Successfully.** \n\nDo `{hl}help` again to get the help menu.")
+            await bot.delete_messages(
+                conv.chat_id, [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id]
+            )
     else:
-        await eor(event, "**⚠️ 𝖤𝖱𝖱𝖮𝖱 !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
+        await eor(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
 
 
-@bot.on(d3vil_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
+@d3vil_cmd(pattern="plinfo(?:\s|$)([\s\S]*)")
 async def d3vilbott(event):
-    if event.fwd_from:
-        return
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await eor(event, str(CMD_HELP[args]))
         else:
-            await eod(event, "**⚠️ 𝖤𝗋𝗋𝗈𝗋 !** \nNeed a module name to show plugin info.")
+            await eod(event, "**⚠️ Error !** \nNeed a module name to show plugin info.")
     else:
         string = ""
         sayfa = [
@@ -70,7 +76,7 @@ async def d3vilbott(event):
         ]
 
         for i in sayfa:
-            string += f"`▶️ `"
+            string += f"`📌 `"
             for sira, a in enumerate(i):
                 string += "`" + str(a)
                 if sira == i.index(i[-1]):
@@ -78,6 +84,15 @@ async def d3vilbott(event):
                 else:
                     string += "`, "
             string += "\n"
-        await eod(event, "Please Specify A Module Name Of Which You Want Info" + "\n\n" + string)
+        await eor(event, "Please Specify A Module Name Of Which You Want Info" + "\n\n" + string)
 
+
+@d3vil_cmd(pattern="cmdinfo(?:\s|$)([\s\S]*)")
+async def cmdinfo(event):
+    cmd = (event.text[9:]).lower()
+    try:
+        info = CMD_INFO[cmd]["info"]
+        await eor(event, f"**• {cmd}:** \n» __{info}__")
+    except KeyError:
+        await eod(event, f"**• No command named:** `{cmd}`")
 
