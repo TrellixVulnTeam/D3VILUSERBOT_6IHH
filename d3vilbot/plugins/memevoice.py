9f1fe36d929b036d47d6e45ff89ec41ba3947a37
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-07 11:10:02 +0530

    Update memevoice.py

--- d3vilbot/plugins/memevoice.py
+++ d3vilbot/plugins/memevoice.py
@@ -1,69 +1,61 @@
-import re
 from . import *
 
-# Credits goes to developer of D3vilBot.
-# This is  first plugin that he made when he released first D3vilBot.
-# Modified to work in groups with inline mode disabled.
-# Added error msg if no voice is found.
-# So please dont remove credit. 
-# You can use it in your repo. But dont remove these lines...
-#Now this plugin on D3VIL BOT
-
-@bot.on(d3vil_cmd(pattern="mev(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="mev(?: |$)(.*)", allow_sudo=True))
-async def nope(d3vilkrish):
-    d3vil = d3vilkrish.pattern_match.group(1)
+@d3vil_cmd(pattern="mev(?:\s|$)([\s\S]*)")
+async def _(event):
+    d3vil = event.text[5:]
+    rply = await event.get_reply_message()
     if not d3vil:
-        if d3vilkrish.is_reply:
-            (await d3vilkrish.get_reply_message()).message
+        if event.is_reply:
+            rply.message
         else:
-            await edit_or_reply(d3vilkrish, "`Sir please give some query to search and download it for you..!`"
-            )
-            return
-
-    troll = await bot.inline_query("TrollVoiceBot", f"{(deEmojify(d3vil))}")
+            return await eod(event, "`Sir please give some query to search and download it for you..!`")
+    troll = await event.client.inline_query("TrollVoiceBot", f"{(deEmojify(d3vil))}")
     if troll:
-        await d3vilkrish.delete()
-        d3vl_ = await troll[0].click(Config.LOGGER_ID)
-        if d3vl_:
-            await bot.send_file(
-                d3vilkrish.chat_id,
-                d3vl_,
+        await event.delete()
+        fucking_ = await troll[0].click(Config.LOGGER_ID)
+        if fucking_:
+            await event.client.send_file(
+                event.chat_id,
+                fucking_,
                 caption="",
+                reply_to=rply,
             )
-        await d3vl_.delete()
+        await fucking_.delete()
     else:
-    	await eod(d3vilkrish, "**Error 404:**  Not Found")
-    	
-@bot.on(d3vil_cmd(pattern="meev(?: |$)(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="meev(?: |$)(.*)", allow_sudo=True))
-async def nope(d3vilkrish):
-    d3vil = d3vilkrish.pattern_match.group(1)
+        await eod(event, "**Error 404:**  Not Found")
+
+
+@d3vil_cmd(pattern="meev(?:\s|$)([\s\S]*)")
+async def _(event):
+    d3vil = event.text[6:]
+    rply = await event.get_reply_message()
     if not d3vil:
-        if d3vilkrish.is_reply:
-            (await d3vilkrish.get_reply_message()).message
+        if event.is_reply:
+            rply.message
         else:
-            await edit_or_reply(d3vilkrish, "`Sir please give some query to search and download it for you..!`"
-            )
-            return
-
-    troll = await bot.inline_query("Myinstantsbot", f"{(deEmojify(d3vil))}")
+            return await eod(event, "`Sir please give some query to search and download it for you..!`")
+    troll = await event.client.inline_query("Myinstantsbot", f"{(deEmojify(d3vil))}")
     if troll:
-        await d3vilkrish.delete()
-        d3vl_ = await troll[0].click(Config.LOGGER_ID)
-        if d3vl_:
-            await bot.send_file(
-                d3vilkrish.chat_id,
-                d3vl_,
+        await event.delete()
+        fucking_ = await troll[0].click(Config.LOGGER_ID)
+        if fucking_:
+            await event.client.send_file(
+                event.chat_id,
+                fucking_,
                 caption="",
+                reply_to=rply,
             )
-        await d3vl_.delete()
+        await fucking_.delete()
     else:
-    	await eod(d3vilkrish, "**Error 404:**  Not Found")
+        await eod(event, "**Error 404:**  Not Found")
 
 
 CmdHelp("memevoice").add_command(
-	"mev", "<query>", "Searches the given meme and sends audio if found."
+    "mev", "<query>", "Searches the given meme and sends audio if found."
 ).add_command(
-	"meev", "<query>", "Same as {hl}mev"
+    "meev", "<query>", "Same as {hl}mev"
+).add_info(
+    "Audio Memes."
+).add_warning(
+    "✅ Harmless Module."
 ).add()
