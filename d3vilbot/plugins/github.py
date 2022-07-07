40a7efd2530a3161c7046774e3802b23f7b1a260
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-07 15:06:41 +0530

    Update github.py

--- d3vilbot/plugins/github.py
+++ d3vilbot/plugins/github.py
@@ -1,49 +1,51 @@
 import os
 import time
-from datetime import datetime
+import datetime
 
 from github import Github
 
 from . import *
 
-GIT_TEMP_DIR = "./userbot/temp/"
+GIT_TEMP_DIR = "./github/"
 
-@bot.on(d3vil_cmd(pattern=r"commit"))
-@bot.on(sudo_cmd(pattern=r"commit"))
+
+@d3vil_cmd(pattern="commit(?:\s|$)([\s\S]*)")
 async def download(event):
-    if event.fwd_from:
-        return
     if Config.GITHUB_ACCESS_TOKEN is None:
-        await eod(event, "`Please ADD Proper Access Token from github.com`")
+        await eod(event, "Please add proper access token from github.com")
         return
     if Config.GIT_REPO_NAME is None:
-        await eod(event, "`Please ADD Proper Github Repo Name of D3vilBot`")
+        await eod(event, "`Please add proper Github Repo Name.`")
         return
+    txts = event.text[8:]
+    splt = txts.split("|")
+    path = splt[0]
+    branch = splt[1] or "master"
     d3vilbot = await eor(event, "Processing ...")
     if not os.path.isdir(GIT_TEMP_DIR):
         os.makedirs(GIT_TEMP_DIR)
-    start = datetime.now()
+    start = datetime.datetime.now()
     reply_message = await event.get_reply_message()
     try:
         time.time()
         print("Downloading to TEMP directory")
-        downloaded_file_name = await bot.download_media(
+        downloaded_file_name = await event.client.download_media(
             reply_message.media, GIT_TEMP_DIR
         )
     except Exception as e:
         await eod(d3vilbot, str(e))
     else:
-        end = datetime.now()
+        end = datetime.datetime.now()
         ms = (end - start).seconds
         await event.delete()
         await d3vilbot.edit(
             "Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms)
         )
         await d3vilbot.edit("Committing to Github....")
-        await git_commit(downloaded_file_name, d3vilbot)
+        await git_commit(downloaded_file_name, path, branch, d3vilbot)
 
 
-async def git_commit(file_name, d3vilbot):
+async def git_commit(file_name, path, branch, d3vilbot):
     content_list = []
     access_token = Config.GITHUB_ACCESS_TOKEN
     g = Github(access_token)
@@ -59,34 +61,32 @@ async def git_commit(file_name, d3vilbot):
     for i in content_list:
         create_file = True
         if i == 'ContentFile(path="' + file_name + '")':
-            return await hellbot.edit("`File Already Exists`")
+            return await d3vilbot.edit("`File Already Exists`")
             create_file = False
-    file_name = "d3vilbot/plugins/" + file_name
+    path = path
+    file_name = file_name
     if create_file == True:
-        file_name = file_name.replace("./userbot/temp/", "")
+        file_name = file_name.replace("./github/", "")
         print(file_name)
         try:
             repo.create_file(
-                file_name, "Uploaded New Plugin", commit_data, branch="master"
+                path, f"Uploaded file {file_name} by Hêllẞø†", commit_data, branch=branch
             )
             print("Committed File")
             ccess = Config.GIT_REPO_NAME
             ccess = ccess.strip()
             await d3vilbot.edit(
-                f"`Commited On Your Github Repo`\n\n[Your STDPLUGINS](https://github.com/{ccess}/tree/master/userbot/plugins/)"
+                f"`Commited On Your Github Repo`\n\n[Your Commit](https://github.com/{ccess}/tree/{branch}/)"
             )
         except:
             print("Cannot Create Plugin")
-            await eod(d3vilbot, "Cannot Upload Plugin")
+            await eod(d3vilbot, "Cannot Upload File")
     else:
         return await eod(d3vilbot, "`Committed Suicide`")
 
 
-@bot.on(d3vil_cmd(pattern="github (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="github (.*)", allow_sudo=True))
+@d3vil_cmd(pattern="github(?:\s|$)([\s\S]*)")
 async def _(event):
-    if event.fwd_from:
-        return
     input_str = event.pattern_match.group(1)
     url = "https://api.github.com/users/{}".format(input_str)
     r = requests.get(url)
@@ -101,7 +101,7 @@ async def _(event):
         location = b["location"]
         bio = b["bio"]
         created_at = b["created_at"]
-        await bot.send_file(
+        await event.client.send_file(
             event.chat_id,
             caption="""Name: [{}]({})
 Type: {}
@@ -123,7 +123,11 @@ Profile Created: {}""".format(
 
 
 CmdHelp("github").add_command(
-  "commit", "<reply to a file>", "Uploads the file on github repo as provided in Heroku Config GIT_REPO_NAME. In short makes a commit to git repo from Userbot"
+  "commit", "<reply to a file> <path>|<branch>", "Uploads the file on github repo as provided in Heroku Config GIT_REPO_NAME. In short makes a commit to git repo from Userbot", "commit ./d3vilbot/plugins/example.py|master"
 ).add_command(
   "github", "<git username>", "Fetches the details of the given git username"
+).add_info(
+  "Github Hecks.."
+).add_warning(
+  "✅ Harmless Module."
 ).add()
