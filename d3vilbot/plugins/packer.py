ada5fdce169c1cda9375849a27f4608e9ca24fe4
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-07 10:59:16 +0530

    Update packer.py

--- d3vilbot/plugins/packer.py
+++ d3vilbot/plugins/packer.py
@@ -4,8 +4,7 @@ import asyncio
 from . import *
 
 
-@bot.on(d3vil_cmd(pattern=r"unpack", outgoing=True))
-@bot.on(sudo_cmd(pattern=r"unpack"))
+@d3vil_cmd(pattern="unpack$")
 async def _(event):
     b = await event.client.download_media(await event.get_reply_message())
     a = open(b, "r")
@@ -15,20 +14,19 @@ async def _(event):
     if len(c) > 4095:
         await a.edit("Telegram Word Limit Of **4095** words exceeded. \n**ABORTING PROCESS !!**")
     else:
-        await event.client.send_message(event.chat_id, f"{c}")
+        await event.client.send_message(event.chat_id, f"{c}", parse_mode=None)
         await a.delete()
     os.remove(b)
 
 
-@bot.on(d3vil_cmd(pattern=r"pack ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern=r"pack ?(.*)", allow_sudo=True))
+@d3vil_cmd(pattern="pack(?:\s|$)([\s\S]*)")
 async def _(event):
     a = await event.get_reply_message()
     input_str = event.pattern_match.group(1)
     b = open(input_str, "w")
     b.write(str(a.message))
     b.close()
-    a = await edit_or_reply(event, f"Packing into `{input_str}`")
+    a = await eor(event, f"Packing into `{input_str}`")
     await asyncio.sleep(2)
     await a.edit(f"Uploading `{input_str}`")
     await asyncio.sleep(2)
@@ -41,4 +39,8 @@ CmdHelp("packer").add_command(
   "unpack", "<reply to a file>", "Read contents of file and send as a telegram message."
 ).add_command(
   "pack", "<reply to text> <filename . extension>", "Packs the text and sends as a file of given extension", "<reply to text> example.py"
+).add_info(
+  "Packer Iz Here !"
+).add_warning(
+  "✅ Harmless Module."
 ).add()
