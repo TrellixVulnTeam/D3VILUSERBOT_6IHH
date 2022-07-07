0c6dd07fb389c4b4953cef694153a1d5aeaf9bf1
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-07 15:38:28 +0530

    Update filter.py

--- d3vilbot/plugins/filter.py
+++ d3vilbot/plugins/filter.py
@@ -1,9 +1,12 @@
 import asyncio
 import re
+
 from telethon import events, utils as ut
 from telethon.tl import types
+
+from ..sql.filter_sql import get_filter, add_filter, remove_filter, get_all_filters, remove_all_filters
+from ..sql.gvar_sql import gvarstat, addgvar, delgvar
 from . import *
-from d3vilbot.sql.filter_sql import get_filter, add_filter, remove_filter, get_all_filters, remove_all_filters
 
 
 DELETE_TIMEOUT = 0
@@ -11,20 +14,35 @@ TYPE_TEXT = 0
 TYPE_PHOTO = 1
 TYPE_DOCUMENT = 2
 
+global last_triggered_filters_1
+last_triggered_filters_1 = {}
 
-global last_triggered_filters
-last_triggered_filters = {}  # pylint:disable=E0602
+global last_triggered_filters_2
+last_triggered_filters_2 = {}
 
-@bot.on(d3vil_cmd(incoming=True))
+global last_triggered_filters_3
+last_triggered_filters_3 = {}
+
+global last_triggered_filters_4
+last_triggered_filters_4 = {}
+
+global last_triggered_filters_5
+last_triggered_filters_5 = {}
+
+
+
+@D1.on(events.NewMessage(incoming=True))
 async def on_snip(event):
-    global last_triggered_filters
+    global last_triggered_filters_1
     name = event.raw_text
-    if event.chat_id in last_triggered_filters:
-        if name in last_triggered_filters[event.chat_id]:
-            # avoid userbot spam
-            # "I demand rights for us bots, we are equal to you humans." -Henri Koivuneva (t.me/UserbotTesting/2698)
+    _id = event.chat_id
+    d3vilkrish, _, _ = await client_id(event)
+    if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+        return
+    snips = get_all_filters(_id)
+    if _id in last_triggered_filters_1:
+        if name in last_triggered_filters_1[_id]:
             return False
-    snips = get_all_filters(event.chat_id)
     if snips:
         for snip in snips:
             pattern = r"( |^|[^\w])" + re.escape(snip.keyword) + r"( |$|[^\w])"
@@ -50,20 +68,199 @@ async def on_snip(event):
                     snip.reply,
                     file=media
                 )
-                if event.chat_id not in last_triggered_filters:
-                    last_triggered_filters[event.chat_id] = []
-                last_triggered_filters[event.chat_id].append(name)
+                if _id not in last_triggered_filters_1:
+                    last_triggered_filters_1[_id] = []
+                last_triggered_filters_1[_id].append(name)
                 await asyncio.sleep(DELETE_TIMEOUT)
-                last_triggered_filters[event.chat_id].remove(name)
+                last_triggered_filters_1[_id].remove(name)
 
 
-@bot.on(d3vil_cmd(pattern=r"filter (.*)"))
-@bot.on(sudo_cmd(pattern=r"filter (.*)", allow_sudo=True))
+if D2:
+    @D2.on(events.NewMessage(incoming=True))
+    async def on_snip(event):
+        global last_triggered_filters_2
+        name = event.raw_text
+        _id = event.chat_id
+        d3vilkrish, _, _ = await client_id(event)
+        if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+            return
+        snips = get_all_filters(_id)
+        if _id in last_triggered_filters_2:
+            if name in last_triggered_filters_2[_id]:
+                return False
+        if snips:
+            for snip in snips:
+                pattern = r"( |^|[^\w])" + re.escape(snip.keyword) + r"( |$|[^\w])"
+                if re.search(pattern, name, flags=re.IGNORECASE):
+                    if snip.snip_type == TYPE_PHOTO:
+                        media = types.InputPhoto(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    elif snip.snip_type == TYPE_DOCUMENT:
+                        media = types.InputDocument(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    else:
+                        media = None
+                    message_id = event.message.id
+                    if event.reply_to_msg_id:
+                        message_id = event.reply_to_msg_id
+                    await event.reply(
+                        snip.reply,
+                        file=media
+                    )
+                    if _id not in last_triggered_filters_2:
+                        last_triggered_filters_2[_id] = []
+                    last_triggered_filters_2[_id].append(name)
+                    await asyncio.sleep(DELETE_TIMEOUT)
+                    last_triggered_filters_2[_id].remove(name)
+
+
+if D3:
+    @D3.on(events.NewMessage(incoming=True))
+    async def on_snip(event):
+        global last_triggered_filters_3
+        name = event.raw_text
+        _id = event.chat_id
+        d3vilkrish, _, _ = await client_id(event)
+        if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+            return
+        snips = get_all_filters(_id)
+        if _id in last_triggered_filters_3:
+            if name in last_triggered_filters_3[_id]:
+                return False
+        if snips:
+            for snip in snips:
+                pattern = r"( |^|[^\w])" + re.escape(snip.keyword) + r"( |$|[^\w])"
+                if re.search(pattern, name, flags=re.IGNORECASE):
+                    if snip.snip_type == TYPE_PHOTO:
+                        media = types.InputPhoto(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    elif snip.snip_type == TYPE_DOCUMENT:
+                        media = types.InputDocument(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    else:
+                        media = None
+                    message_id = event.message.id
+                    if event.reply_to_msg_id:
+                        message_id = event.reply_to_msg_id
+                    await event.reply(
+                        snip.reply,
+                        file=media
+                    )
+                    if _id not in last_triggered_filters_3:
+                        last_triggered_filters_3[_id] = []
+                    last_triggered_filters_3[_id].append(name)
+                    await asyncio.sleep(DELETE_TIMEOUT)
+                    last_triggered_filters_3[_id].remove(name)
+
+
+if D4:
+    @D4.on(events.NewMessage(incoming=True))
+    async def on_snip(event):
+        global last_triggered_filters_4
+        name = event.raw_text
+        _id = event.chat_id
+        d3vilkrish, _, _ = await client_id(event)
+        if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+            return
+        snips = get_all_filters(_id)
+        if _id in last_triggered_filters_4:
+            if name in last_triggered_filters_4[_id]:
+                return False
+        if snips:
+            for snip in snips:
+                pattern = r"( |^|[^\w])" + re.escape(snip.keyword) + r"( |$|[^\w])"
+                if re.search(pattern, name, flags=re.IGNORECASE):
+                    if snip.snip_type == TYPE_PHOTO:
+                        media = types.InputPhoto(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    elif snip.snip_type == TYPE_DOCUMENT:
+                        media = types.InputDocument(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    else:
+                        media = None
+                    message_id = event.message.id
+                    if event.reply_to_msg_id:
+                        message_id = event.reply_to_msg_id
+                    await event.reply(
+                        snip.reply,
+                        file=media
+                    )
+                    if _id not in last_triggered_filters_4:
+                        last_triggered_filters_4[_id] = []
+                    last_triggered_filters_4[_id].append(name)
+                    await asyncio.sleep(DELETE_TIMEOUT)
+                    last_triggered_filters_4[_id].remove(name)
+
+
+if D5:
+    @D5.on(events.NewMessage(incoming=True))
+    async def on_snip(event):
+        global last_triggered_filters_5
+        name = event.raw_text
+        _id = event.chat_id
+        d3vilkrish, _, _ = await client_id(event)
+        if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+            return
+        snips = get_all_filters(_id)
+        if _id in last_triggered_filters_5:
+            if name in last_triggered_filters_5[_id]:
+                return False
+        if snips:
+            for snip in snips:
+                pattern = r"( |^|[^\w])" + re.escape(snip.keyword) + r"( |$|[^\w])"
+                if re.search(pattern, name, flags=re.IGNORECASE):
+                    if snip.snip_type == TYPE_PHOTO:
+                        media = types.InputPhoto(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    elif snip.snip_type == TYPE_DOCUMENT:
+                        media = types.InputDocument(
+                            int(snip.media_id),
+                            int(snip.media_access_hash),
+                            snip.media_file_reference
+                        )
+                    else:
+                        media = None
+                    message_id = event.message.id
+                    if event.reply_to_msg_id:
+                        message_id = event.reply_to_msg_id
+                    await event.reply(
+                        snip.reply,
+                        file=media
+                    )
+                    if _id not in last_triggered_filters_5:
+                        last_triggered_filters_5[_id] = []
+                    last_triggered_filters_5[_id].append(name)
+                    await asyncio.sleep(DELETE_TIMEOUT)
+                    last_triggered_filters_5[_id].remove(name)
+
+
+@d3vil_cmd(pattern="filter(?:\s|$)([\s\S]*)")
 async def on_snip_save(event):
-    if event.fwd_from:
-        return
     name = event.pattern_match.group(1)
     msg = await event.get_reply_message()
+    d3vilkrish, _, d3vil_mention = await client_id(event)
+    _id = event.chat_id
     if msg:
         snip = {'type': TYPE_TEXT, 'text': msg.message or ''}
         if msg.media:
@@ -78,18 +275,20 @@ async def on_snip_save(event):
                 snip['id'] = media.id
                 snip['hash'] = media.access_hash
                 snip['fr'] = media.file_reference
-        add_filter(event.chat_id, name, snip['text'], snip['type'], snip.get('id'), snip.get('hash'), snip.get('fr'))
-        await eod(event, f"**Filter** `{name}` **saved successfully. Get it with** `{name}`", 7)
+        add_filter(_id, name, snip['text'], snip['type'], snip.get('id'), snip.get('hash'), snip.get('fr'))
+        addgvar(f"FILTER_{d3vilkrish}_{str(_id)[1:]}", "TRUE")
+        await eod(event, f"**Filter Saved!** \n\n__• Client:__ {d3vil_mention} \n__• Keyword:__ {name}")
     else:
         await eod(event, f"Reply to a message with `{hl}filter keyword` to save the filter")
 
 
-@bot.on(d3vil_cmd(pattern="filters$"))
-@bot.on(sudo_cmd(pattern="filters$", allow_sudo=True))
+@d3vil_cmd(pattern="filters$")
 async def on_snip_list(event):
-    if event.fwd_from:
-        return
-    all_snips = get_all_filters(event.chat_id)
+    d3vilkrish, _, _ = await client_id(event)
+    _id = event.chat_id
+    if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+        return await eod(event, f"No filters. Start saving using `{hl}filter`")
+    all_snips = get_all_filters(_id)
     OUT_STR = "**Available Filters in the Current Chat :** \n"
     if len(all_snips) > 0:
         for a_snip in all_snips:
@@ -99,7 +298,7 @@ async def on_snip_list(event):
     if len(OUT_STR) > 4096:
         with io.BytesIO(str.encode(OUT_STR)) as out_file:
             out_file.name = "filters.text"
-            await bot.send_file(
+            await event.client.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -109,26 +308,35 @@ async def on_snip_list(event):
             )
             await event.delete()
     else:
-        await eod(event, OUT_STR)
+        await eor(event, OUT_STR)
 
 
-@bot.on(d3vil_cmd(pattern="stop (.*)"))
-@bot.on(sudo_cmd(pattern="stop (.*)", allow_sudo=True))
+@d3vil_cmd(pattern="stop(?:\s|$)([\s\S]*)")
 async def on_snip_delete(event):
-    if event.fwd_from:
-        return
     name = event.pattern_match.group(1)
-    remove_filter(event.chat_id, name)
-    await eod(event, f"Filter `{name}` deleted successfully")
+    d3vilkrish, _, d3vil_mention = await client_id(event)
+    _id = event.chat_id
+    if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+        return await eod(event, "There aren't any filter in chat!")
+    try:
+        remove_filter(_id, name)
+        await eod(event, f"**Filter Deleted!** \n\n__• Client:__ {d3vil_mention} \n__• Keyword:__ {name}")
+    except Exception as e:
+        await eod(event, f"**ERROR !!** \n\n`{e}`")
 
 
-@bot.on(d3vil_cmd(pattern="rmallfilters$"))
-@bot.on(sudo_cmd(pattern="rmallfilters$", allow_sudo=True))
+@d3vil_cmd(pattern="rmallfilters$")
 async def on_all_snip_delete(event):
-    if event.fwd_from:
-        return
-    remove_all_filters(event.chat_id)
-    await eor(event, f"**All the Filters in current chat deleted successfully**")
+    d3vilkrish, _, _ = await client_id(event)
+    _id = event.chat_id
+    if not gvarstat(f"FILTER_{d3vilkrish}_{str(_id)[1:]}"):
+        return await eod(event, "No filters in this chat!")
+    try:
+        delgvar(f"FILTER_{d3vilkrish}_{str(_id)[1:]}")
+        remove_all_filters(_id)
+        await eod(event, f"**All the Filters in current chat deleted successfully**")
+    except Exception as e:
+        await eod(event, f"**ERORR !!** \n\n{str(e)}")
 
 
 CmdHelp("filter").add_command(
@@ -139,4 +347,8 @@ CmdHelp("filter").add_command(
   "rmallfilters", None, "Deletes all the filter saved in a chat."
 ).add_command(
   "stop", "keyword of saved filter", "Stops reply to the keyword mentioned."
+).add_info(
+  "Save Filters."
+).add_warning(
+  "✅ Harmless Module."
 ).add()
