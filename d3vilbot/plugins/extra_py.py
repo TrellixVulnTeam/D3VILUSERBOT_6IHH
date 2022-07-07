7339df0c9b98b8e1c0a062d0d47522e79c3fc6b2
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-07 19:58:07 +0530

    Update extra_py.py

--- d3vilbot/plugins/extra_py.py
+++ d3vilbot/plugins/extra_py.py
@@ -5,78 +5,59 @@ from telethon.tl.types import InputMessagesFilterDocument
 
 from . import *
 
-@bot.on(d3vil_cmd(pattern="extdl$", outgoing=True))
-@bot.on(sudo_cmd(pattern="extdl$", allow_sudo=True))
+
+@d3vil_cmd(pattern="extdl$")
 async def install(event):
-    if event.fwd_from:
-        return
     chat = Config.PLUGIN_CHANNEL
-    documentss = await bot.get_messages(chat, None, filter=InputMessagesFilterDocument)
+    documentss = await event.client.get_messages(chat, None, filter=InputMessagesFilterDocument)
     total = int(documentss.total)
     total_doxx = range(0, total)
-    await event.delete()
+    d3vil_ = await eor(event, "Installing plugins from Plugin Channel...")
+    d3vil = "**Installed Plugins :**\n\n"
     for ixo in total_doxx:
         mxo = documentss[ixo].id
         downloaded_file_name = await event.client.download_media(
-            await bot.get_messages(chat, ids=mxo), "d3vilbot/plugins/"
+            await event.client.get_messages(chat, ids=mxo), "d3vilbot/plugins/"
         )
         if "(" not in downloaded_file_name:
             path1 = Path(downloaded_file_name)
             shortname = path1.stem
             load_module(shortname.replace(".py", ""))
-            await bot.send_message(
-                event.chat_id,
-                "Installed Plugin `{}` successfully.".format(
-                    os.path.basename(downloaded_file_name)
-                ),
-            )
+            d3vil += "• __Installed Plugin__ `{}` __successfully.__\n".format(os.path.basename(downloaded_file_name))
         else:
-            await bot.send_message(
-                event.chat_id,
-                "Plugin `{}` has been pre-installed and cannot be installed.".format(
-                    os.path.basename(downloaded_file_name)
-                ),
-            )
+            d3vil += "• __Plugin__ `{}` __has been pre-installed and cannot be installed.__\n".format(os.path.basename(downloaded_file_name))
+    await d3vil_.edit(d3vil)
 
 
-@bot.on(d3vil_cmd(pattern=r"installall (.*)"))
-@bot.on(sudo_cmd(pattern=r"installall (.*)", allow_sudo=True))
+@d3vil_cmd(pattern="installall ([\s\S]*)")
 async def install(event):
-    if event.fwd_from:
-        return
     chat = event.pattern_match.group(1)
-    d3vil = await eor(event, f"Starting To Install Plugins From {chat} !!"
-    )
-    documentss = await bot.get_messages(chat, None, filter=InputMessagesFilterDocument)
+    d3vil_ = await eor(event, f"Starting To Install Plugins From {chat} !!")
+    d3vil = f"**Installed Plugins From {chat} :**\n\n"
+    documentss = await event.client.get_messages(chat, None, filter=InputMessagesFilterDocument)
     total = int(documentss.total)
     total_doxx = range(0, total)
     for ixo in total_doxx:
         mxo = documentss[ixo].id
         downloaded_file_name = await event.client.download_media(
-            await bot.get_messages(chat, ids=mxo), "d3vilbot/plugins/"
+            await event.client.get_messages(chat, ids=mxo), "d3vilbot/plugins/"
         )
         if "(" not in downloaded_file_name:
             path1 = Path(downloaded_file_name)
             shortname = path1.stem
             load_module(shortname.replace(".py", ""))
-            sed = f"Installing Plugins From {chat}"
-            logger.info(sed)
-            await bot.send_message(
-                event.chat_id,
-                "Installed Plugin `{}` successfully.".format(
-                    os.path.basename(downloaded_file_name)
-                ),
-            )
+            d3vil += "• __Installed Plugin__ `{}` __successfully.__\n".format(os.path.basename(downloaded_file_name))
         else:
-            await bot.send_message(
-                event.chat_id,
-                "Plugin `{}` has been pre-installed and cannot be installed.".format(
-                    os.path.basename(downloaded_file_name)
-                ),
-            )
+            d3vil += "• __Plugin__ `{}` __has been pre-installed and cannot be installed.__\n".format(os.path.basename(downloaded_file_name))
+    await d3vil_.edit(d3vil)
+
 
 CmdHelp("extra_py").add_command(
   "extdl", None, "Installs all plugins from the channal which id is in PLUGIN_CHANNEL Configiable"
 ).add_command(
   "installall", "<channel/grp username>", "Installs all the plugins in provided channel / group. (May get floodwait error)"
+).add_info(
+  "Extra Plugins."
+).add_warning(
+  "Don't Install plugins from Unknown channel."
 ).add()
