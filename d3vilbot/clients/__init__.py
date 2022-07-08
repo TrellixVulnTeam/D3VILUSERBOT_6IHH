dc8f1378ed036a3d231525d4a29306e9d191a1a8
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-08 20:54:20 +0530

    Update __init__.py

--- d3vilbot/clients/__init__.py
+++ d3vilbot/clients/__init__.py
@@ -3,25 +3,3 @@ from .decs import d3vil_cmd, d3vil_handler
 from .session import D3vil, D2, D3, D4, D5, D3vilBot
 #########
 ###LAWDA IMPORTING #### IN YOUR MOUTH ######
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from d3vilbot.config import Config
-
-
-def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-try:
-    BASE = declarative_base()
-    SESSION = start()
-except AttributeError as e:
-    # this is a dirty way for the work-around required for #23
-    print("DB_URI is not configured. Features depending on the database might have issues.")
-    print(str(e))
