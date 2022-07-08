361e3060fdc75085a5d0e3e95bbb13ec4167ab1d
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-08 19:19:25 +0530

    Update __init__.py

--- d3vilbot/__init__.py
+++ d3vilbot/__init__.py
@@ -8,7 +8,7 @@ from logging import DEBUG, INFO, basicConfig, getLogger
 
 from d3vilbot.clients.session import D2, D3, D4, D5, D3vil, D3vilBot
 from d3vilbot.config import Config
-
+from d3vilbot.clients import *
 
 StartTime = time.time()
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
