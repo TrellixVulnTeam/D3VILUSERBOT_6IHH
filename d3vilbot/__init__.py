0d9d6e39b15057ebca8c1c7d566f139edc74440d
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-07-10 22:35:59 +0530

    try 2nd

---
+++ d3vilbot/__init__.py
@@ -0,0 +1,16 @@
+# global variables
+CMD_LIST = {}
+CMD_HELP = {}
+CMD_HELP_BOT = {}
+CMD_INFO = {}
+INT_PLUG = ""
+LOAD_PLUG = {}
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+ISAFK = False
+AFKREASON = None
+SUDO_LIST = {}
+
+
