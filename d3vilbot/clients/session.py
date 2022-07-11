abfaff151442de0225d8a35881131b7d4e296e5e
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-11 14:27:21 +0530

    Update session.py

--- d3vilbot/clients/session.py
+++ d3vilbot/clients/session.py
@@ -4,8 +4,9 @@ from telethon import TelegramClient
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 
-from d3vilbot.config import *
-
+#from d3vilbot.config import Config 
+from d3vilbot import config 
+import Config
 
 if Config.D3VILBOT_SESSION:
     session = StringSession(str(Config.D3VILBOT_SESSION))
