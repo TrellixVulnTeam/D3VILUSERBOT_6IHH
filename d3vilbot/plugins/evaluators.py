d6176ae090795f785e76de6efe47f730acc492f1
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-15 11:06:36 +0530

    Update evaluators.py

--- d3vilbot/plugins/evaluators.py
+++ d3vilbot/plugins/evaluators.py
@@ -73,7 +73,7 @@ async def _(event):
     else:
         evaluation = "Success"
     final_output = f"•  Eval : \n`{cmd}` \n\n•  Result : \n `{evaluation}` \n"
-    await eor(d3vilevent, "**Eval Command Executed**. `{final_output}`")
+    await eor(d3vilevent, "**Eval Command Executed.Check out LOGGER for result.**")
     await event.client.send_message(
         lg_id,
         f"#EVAL \n\nEval command was executed sucessfully. \n\n{final_output}",
