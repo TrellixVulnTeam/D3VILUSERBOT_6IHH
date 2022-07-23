92f341ca1cb102567ce23057d3a6a9865b0bf0dd
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-23 10:23:19 +0530

    Update spam.py

--- d3vilbot/assistant/spam.py
+++ d3vilbot/assistant/spam.py
@@ -6,10 +6,31 @@ MY_USERS = bot.uid
 a = False
 
 @tgbot.on(events.NewMessage(pattern="^/spam"))
-async def spammer(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        message = e.text
-        counter = int(message[6:8])
-        spam_message = str(e.text[8:])
-        await asyncio.wait([e.respond(spam_message) for i in range(counter)])
-        await e.delete()
+async def spam(e):
+        if e.sender_id in TELE_NAME:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        d3vil = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(d3vil) == 2:
+            message = str(d3vil[1])
+            counter = int(d3vil[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        elif e.reply_to_msg_id and smex.media:  
+            counter = int(d3vil[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            for _ in range(counter):
+                smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                await gifspam(e, smex)  
+        elif e.reply_to_msg_id and smex.text:
+            message = smex.text
+            counter = int(d3vil[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+            
