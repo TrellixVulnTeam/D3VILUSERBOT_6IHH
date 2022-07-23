93f121aef9c0983e37f305e90017507a192d5e5a
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-23 09:43:32 +0530

    Create spam.py

---
+++ d3vilbot/assistant/spam.py
@@ -0,0 +1,23 @@
+from telethon import events
+
+a = False
+
+@tgbot.on(events.NewMessage(pattern="^/spam"))
+async def spam(e):
+    if e.sender_id in MY_USERS:
+        try:
+            text = e.raw_text
+            counts = int(text[6:8])
+            spam = text[8:]
+            message = str(spam)
+            if e.is_reply == True:
+                replied = await e.get_reply_message()
+                replied_message = replied.message
+                for i in range(counts):
+                    await e.client.send_message(e.chat_id, replied_message)
+            else:
+                for i in range(counts):
+                    await e.client.send_message(e.chat_id, message)
+        except Exception as er:
+            print(er)
+            await e.client.send_message(e.chat_id, "Something went wrong!")
