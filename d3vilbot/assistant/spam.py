4a71de36327dc692635198eaf80957dd81891a25
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2022-07-23 10:00:47 +0530

    Update spam.py

--- d3vilbot/assistant/spam.py
+++ d3vilbot/assistant/spam.py
@@ -6,21 +6,10 @@ MY_USERS = bot.uid
 a = False
 
 @tgbot.on(events.NewMessage(pattern="^/spam"))
-async def spam(e):
-    if e.sender_id in MY_USERS:
-        try:
-            text = e.raw_text
-            counts = int(text[6:8])
-            spam = text[8:]
-            message = str(spam)
-            if e.is_reply == True:
-                replied = await e.get_reply_message()
-                replied_message = replied.message
-                for i in range(counts):
-                    await e.client.send_message(e.chat_id, replied_message)
-            else:
-                for i in range(counts):
-                    await e.client.send_message(e.chat_id, message)
-        except Exception as er:
-            print(er)
-            await e.client.send_message(e.chat_id, "Something went wrong!")
+async def spammer(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[6:8])
+        spam_message = str(e.text[8:])
+        await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+        await e.delete()
